f4fcde0b81f7a482c9306c3219e3590428a6060f
danipereiracodes <danipg.dev@gmail.com>
2024-04-15 13:31:02 +0200

    01-feature-create-data-store/add form data to global state

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -20,39 +20,34 @@ const MainContent: React.FC = () => {
     switch (currentStep) {
       case 1:
         return (
-          <div className='z-20 backdrop-blur-sm bg-white/30 rounded-xl p-8 w-[500px] h-[500px] text-black font-extrabold text-2xl'>
+          <div className='transition-opacity ease-in-out delay-150 z-20 backdrop-blur-sm bg-white/30 rounded-xl p-8 w-[350px] md:w-[500px] lg:w-[750px] text-black font-extrabold text-2xl'>
             <StepOneForm
               title='Some information about you'
               gridCols='grid-cols-2'
-              onHandleSubmit={handleSubmit}
+              onNextStep={handleSubmit}
             />
           </div>
         );
       case 2:
         return (
-          <div className='z-20 backdrop-blur-sm bg-white/30 rounded-xl p-8 w-[500px] h-[500px] text-black font-extrabold text-2xl'>
+          <div className='z-20 backdrop-blur-sm bg-white/30 rounded-xl p-8 max-w-2/3 min-w-1/3 text-black font-extrabold text-2xl'>
             <StepOneForm
               title='Tell us about your diet or alergies'
               gridCols='grid-cols-1'
-              onHandleSubmit={handleSubmit}
             />
           </div>
         );
 
       case 3:
         return (
-          <div className='z-20 backdrop-blur-sm bg-white/30 rounded-xl p-8 w-[500px] h-[500px] text-black font-extrabold text-2xl'>
-            <StepOneForm
-              title='How often do you exercise?'
-              gridCols='1'
-              onHandleSubmit={handleSubmit}
-            />
+          <div className='z-20 backdrop-blur-sm bg-white/30 rounded-xl p-8 max-w-2/3 min-w-1/3 text-black font-extrabold text-2xl'>
+            <StepOneForm title='How often do you exercise?' gridCols='1' />
           </div>
         );
 
       case 4:
         return (
-          <div className='z-20 backdrop-blur-sm bg-white/30 rounded-xl p-8 w-[500px] h-[500px] text-black font-extrabold text-2xl'>
+          <div className='z-20 backdrop-blur-sm bg-white/30 rounded-xl p-8 max-w-2/3 min-w-1/3 text-black font-extrabold text-2xl'>
             <button onClick={() => setStep(0)}>Share</button>
           </div>
         );
