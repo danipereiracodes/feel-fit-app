0d17bff48a28c6547f3403391e44434ec4b02363
danipereiracodes <danipg.dev@gmail.com>
2024-04-15 18:29:34 +0200

    create custom input todo fix register error

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useStepStore } from '../store/StepStore';
-import StepOneForm from './form-steps/StepOneForm';
 import { healtData } from '../lib/data/healthTips';
+import Steps from './Steps';
+import myBulb from '../components/icons/bulb.svg';
 
 const fetchData = () => {
   const shuffledhealthData = healtData.sort(() => Math.random() - 0.5);
@@ -17,65 +18,6 @@ const MainContent: React.FC = () => {
     setTips(fetchData()[0]);
   }, []);
 
-  const HomeBanner = () => {
-    return (
-      <div className='z-20 flex flex-col gap-4 items-center text-center w-2/3 text-white text-2xl font-bold'>
-        <h1>
-          Get your weekly meal prep and exercise plan{' '}
-          <span className='font-bold underline'>in seconds</span>, starting now!
-        </h1>
-      </div>
-    );
-  };
-
-  const RenderSteps = ({ currentStep }: { currentStep: number }) => {
-    switch (currentStep) {
-      case 1:
-        return (
-          <div className='transition-opacity ease-in-out delay-150 z-20 backdrop-blur-sm bg-white/30 rounded-xl p-8 w-[350px] md:w-[500px] lg:w-[750px] text-black font-extrabold text-2xl'>
-            <StepOneForm
-              title='Some information about you'
-              gridCols='grid-cols-2'
-              onNextStep={handleSubmit}
-            />
-          </div>
-        );
-      case 2:
-        return (
-          <div className='z-20 backdrop-blur-sm bg-white/30 rounded-xl p-8 max-w-2/3 min-w-1/3 text-black font-extrabold text-2xl'>
-            <StepOneForm
-              title='Tell us about your diet or alergies'
-              gridCols='grid-cols-1'
-              onNextStep={handleSubmit}
-            />
-          </div>
-        );
-
-      case 3:
-        return (
-          <div className='z-20 backdrop-blur-sm bg-white/30 rounded-xl p-8 max-w-2/3 min-w-1/3 text-black font-extrabold text-2xl'>
-            <StepOneForm
-              title='How often do you exercise?'
-              gridCols='1'
-              onNextStep={handleSubmit}
-            />
-          </div>
-        );
-
-      case 4:
-        return (
-          <div className='z-20 backdrop-blur-sm bg-white/30 rounded-xl p-8 max-w-2/3 min-w-1/3 text-black font-extrabold text-2xl'>
-            <button onClick={() => setStep(0)}>Share</button>
-          </div>
-        );
-
-      default:
-        setStep(0);
-
-        return HomeBanner();
-    }
-  };
-
   const handleSubmit = () => {
     setStep(step + 1);
   };
@@ -84,7 +26,7 @@ const MainContent: React.FC = () => {
     <section className='relative flex flex-col gap-8 items-center justify-center bg-[url("/images/background/fitness_background.avif")] bg-center bg-cover bg-no-repeat'>
       <div className='absolute w-full h-full inset-0 bg-banner-overlay'></div>
 
-      <RenderSteps currentStep={step} />
+      <Steps currentStep={step} onSubmit={handleSubmit} />
 
       {step === 0 && (
         <div className='flex items-center gap-4 z-20 text-white font-normal text-md'>
@@ -101,23 +43,7 @@ const MainContent: React.FC = () => {
       )}
       <div className='fixed top-8 left-24 w-[150px] h-[150px] flex flex-wrap rounded-xl bg-black text-white p-4 text-ellipsis line-clamp-3'>
         <div className=' relative  flex items-center no-wrap '>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='40'
-            height='40'
-            viewBox='0 0 24 24'
-            fill='none'
-            stroke='currentColor'
-            stroke-width='2'
-            stroke-linecap='round'
-            stroke-linejoin='round'
-            className='absolute top-0 icon icon-tabler icons-tabler-outline icon-tabler-bulb'
-          >
-            <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-            <path d='M3 12h1m8 -9v1m8 8h1m-15.4 -6.4l.7 .7m12.1 -.7l-.7 .7' />
-            <path d='M9 16a5 5 0 1 1 6 0a3.5 3.5 0 0 0 -1 3a2 2 0 0 1 -4 0a3.5 3.5 0 0 0 -1 -3' />
-            <path d='M9.7 17l4.6 0' />
-          </svg>{' '}
+          <img src={myBulb} alt='bulb'></img>{' '}
           <span className='mt-8'>{tips}</span>
         </div>
       </div>
---
+++ src/components/Steps.tsx
@@ -0,0 +1,60 @@
+import { useStepStore } from '../store/StepStore';
+import HomeBanner from './HomeBanner';
+import CustomForm from './form-steps/CustomForm';
+
+interface StepsProps {
+  currentStep: number;
+  onSubmit: () => void;
+}
+
+const Steps: React.FC<StepsProps> = ({ currentStep, onSubmit }) => {
+  const setStep = useStepStore((state) => state.addStep);
+
+  switch (currentStep) {
+    case 1:
+      return (
+        <div className='transition-opacity ease-in-out delay-150 z-20 backdrop-blur-sm bg-white/30 rounded-xl p-8 w-[350px] md:w-[500px] lg:w-[750px] text-black font-extrabold text-2xl'>
+          <CustomForm
+            title='Some information about you'
+            gridCols='grid-cols-2'
+            onNextStep={onSubmit}
+          />
+        </div>
+      );
+    case 2:
+      return (
+        <div className='z-20 backdrop-blur-sm bg-white/30 rounded-xl p-8 max-w-2/3 min-w-1/3 text-black font-extrabold text-2xl'>
+          <CustomForm
+            title='Tell us about your diet or alergies'
+            gridCols='grid-cols-1'
+            onNextStep={onSubmit}
+          />
+        </div>
+      );
+
+    case 3:
+      return (
+        <div className='z-20 backdrop-blur-sm bg-white/30 rounded-xl p-8 max-w-2/3 min-w-1/3 text-black font-extrabold text-2xl'>
+          <CustomForm
+            title='How often do you exercise?'
+            gridCols='1'
+            onNextStep={onSubmit}
+          />
+        </div>
+      );
+
+    case 4:
+      return (
+        <div className='z-20 backdrop-blur-sm bg-white/30 rounded-xl p-8 max-w-2/3 min-w-1/3 text-black font-extrabold text-2xl'>
+          <button onClick={() => setStep(0)}>Share</button>
+        </div>
+      );
+
+    default:
+      setStep(0);
+
+      return <HomeBanner />;
+  }
+};
+
+export default Steps;
