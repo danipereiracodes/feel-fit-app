548d5daeea108d30409c353eeb670bdd766825ea
danipereiracodes <danipg.dev@gmail.com>
2024-04-15 13:53:14 +0200

    01-feature-create-data-store/add login button and footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,10 @@
 const Footer: React.FC = () => {
   return (
     <footer className='flex items-center justify-center bg-primary-dark text-primary-gray text-xl font-extrabold'>
-      <span>Footer Info</span>
+      <span>
+        Web App Developed by danipereira.dev for the "El Rincon Del Dev
+        Hackathon"
+      </span>
     </footer>
   );
 };
