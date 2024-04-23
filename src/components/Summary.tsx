dd67e89082dbc5aaa04d3349af3c635bdbdca58b
danipereiracodes <danipg.dev@gmail.com>
2024-04-23 18:48:40 +0200

    01-feature-step-4-summary/ edit UI styles and add goals enum

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,12 +1,15 @@
 /* import { mockInputValues } from '../mocks/userDataMock'; */
+import { useEffect, useState } from 'react';
 import { useStepStore } from '../store/StepStore';
 import Button from './buttons/Buton';
 
 interface SummaryProps {
   onPrevStep: () => void;
+  onNextStep: () => void;
 }
 
-const Summary: React.FC<SummaryProps> = ({ onPrevStep }) => {
+const Summary: React.FC<SummaryProps> = ({ onPrevStep, onNextStep }) => {
+  const [loading, setLoading] = useState(false);
   /* const {
     name,
     lastname,
@@ -25,8 +28,6 @@ const Summary: React.FC<SummaryProps> = ({ onPrevStep }) => {
 
   const data = useStepStore((state) => state.data);
   const {
-    name,
-    lastname,
     age,
     gender,
     height,
@@ -38,117 +39,134 @@ const Summary: React.FC<SummaryProps> = ({ onPrevStep }) => {
     diet,
     exerciseExp,
     exerciseFreq,
+    goal,
   } = data;
 
-  return (
-    <>
-      <table className='w-full  text-md '>
-        <tbody className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-2'>
-          <tr className='flex flex-col border border-1 '>
-            <td className='px-4 bg-bright-secondary w-full'>name</td>
-            <td className='px-4 py-2 sm:px-6 sm:py-4 whitespace-nowrap bg-white'>
-              {name}
+  useEffect(() => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  return loading ? (
+    <div className='loader'></div>
+  ) : (
+    <section className='transition-opacity ease-in-out delay-150 z-20 bg-white/50 p-8 w-[350px] md:w-[500px] lg:w-[750px] text-black text-lg'>
+      <table className='  text-md'>
+        <tbody className=' grid grid-cols-2 md:grid-cols-4 gap-2'>
+          {/* <tr className='flex flex-col '>
+            <td className='px-4 bg-gray-700 text-white font-bold text-black'>
+              name
             </td>
+            <td className='px-4 py-2 '>{name}</td>
           </tr>
-          <tr className='flex flex-col border border-1  '>
-            <td className='bg-bright-secondary px-4 '>Lastname</td>
-            <td className='px-4 py-2 sm:px-6 sm:py-4 whitespace-nowrap bg-white h-full'>
-              {lastname}
+          <tr className='flex flex-col  '>
+            <td className=' px-4 bg-gray-700 text-white font-bold text-black'>
+              Lastname
             </td>
+            <td className='px-4 py-2  h-full'>{lastname}</td>
+          </tr> */}
+          <tr className='flex flex-col  '>
+            <td className=' px-4 bg-gray-700 text-white font-bold text-black'>
+              Age
+            </td>
+            <td className='px-4 py-2  h-full'>{age}</td>
           </tr>
-          <tr className='flex flex-col border border-1  '>
-            <td className='bg-bright-secondary px-4 '>Age</td>
-            <td className='px-4 py-2 sm:px-6 sm:py-4 whitespace-nowrap bg-white h-full'>
-              {age}
+          <tr className='flex flex-col  '>
+            <td className=' px-4 bg-gray-700 text-white font-bold text-black'>
+              Gender
             </td>
+            <td className='px-4 py-2  h-full'>{gender}</td>
           </tr>
-          <tr className='flex flex-col border border-1  '>
-            <td className='bg-bright-secondary px-4 '>Gender</td>
-            <td className='px-4 py-2 sm:px-6 sm:py-4 whitespace-nowrap bg-white h-full'>
-              {gender}
+          <tr className='flex flex-col  '>
+            <td className=' px-4 bg-gray-700 text-white font-bold text-black'>
+              Height
             </td>
+            <td className='px-4 py-2  h-full'>{height}</td>
           </tr>
-          <tr className='flex flex-col border border-1  '>
-            <td className='bg-bright-secondary px-4 '>Height</td>
-            <td className='px-4 py-2 sm:px-6 sm:py-4 whitespace-nowrap bg-white h-full'>
-              {height}
+          <tr className='flex flex-col  '>
+            <td className=' px-4 bg-gray-700 text-white font-bold text-black'>
+              Weight
             </td>
+            <td className='px-4 py-2  h-full'>{weight}</td>
           </tr>
-          <tr className='flex flex-col border border-1  '>
-            <td className='bg-bright-secondary px-4 '>Weight</td>
-            <td className='px-4 py-2 sm:px-6 sm:py-4 whitespace-nowrap bg-white h-full'>
-              {weight}
+          <tr className='flex flex-col  '>
+            <td className=' px-4 bg-gray-700 text-white font-bold text-black '>
+              Diet
             </td>
+            <td className='px-4 py-2  h-full'>{diet}</td>
           </tr>
-          <tr className='flex flex-col border border-1  '>
-            <td className='bg-bright-secondary px-4  '>Diet</td>
-            <td className='px-4 py-2 sm:px-6 sm:py-4 whitespace-nowrap bg-white h-full'>
-              {diet}
+          <tr className='flex flex-col  '>
+            <td className=' px-4 bg-gray-700 text-white font-bold text-black'>
+              Fasting
             </td>
-          </tr>
-          <tr className='flex flex-col border border-1  '>
-            <td className='bg-bright-secondary px-4 '>Fasting</td>
-            <td className='px-4 py-2 sm:px-6 sm:py-4 whitespace-nowrap bg-white h-full'>
-              {fasting ? 'Yes' : 'No'}
-            </td>
+            <td className='px-4 py-2  h-full'>{fasting ? 'Yes' : 'No'}</td>
           </tr>
           {fastingFreq.length ? (
-            <tr className='flex flex-col border border-1  '>
-              <td className='bg-bright-secondary px-4 '>Fasting freq</td>
-              <td className='px-4 py-2 sm:px-6 sm:py-4 whitespace-nowrap bg-white h-full'>
-                {fastingFreq}
+            <tr className='flex flex-col  '>
+              <td className=' px-4 bg-gray-700 text-white font-bold text-black'>
+                Fasting freq
               </td>
+              <td className='px-4 py-2  h-full'>{fastingFreq}</td>
             </tr>
           ) : null}
           {allergies !== '' ? (
-            <tr className='flex flex-col border border-1  '>
-              <td className='bg-bright-secondary px-4 '>Allergies</td>
-              <td className='px-4 py-2 sm:px-6 sm:py-4 whitespace-nowrap bg-white h-full overflow-x-auto'>
-                {allergies}
+            <tr className='flex flex-col  '>
+              <td className=' px-4 bg-gray-700 text-white font-bold text-black'>
+                Allergies
               </td>
+              <td className='px-4 py-2  h-full truncate'>{allergies}</td>
             </tr>
           ) : null}
-          <tr className='flex flex-col border border-1'>
-            <td className='bg-bright-secondary px-4 '>Exercise Exp</td>
-            <td className='px-4 py-2 sm:px-6 sm:py-4 whitespace-nowrap bg-white h-full overflow-x-auto'>
-              {exerciseExp}
+          <tr className='flex flex-col'>
+            <td className=' px-4 bg-gray-700 text-white font-bold text-black'>
+              Exercise Exp
             </td>
+            <td className='px-4 py-2  h-full truncate'>{exerciseExp}</td>
           </tr>
-          <tr className='flex flex-col border border-1'>
-            <td className='bg-bright-secondary px-4 '>Exercise Freq</td>
-            <td className='px-4 py-2 sm:px-6 sm:py-4 whitespace-nowrap bg-white h-full overflow-x-auto'>
-              {exerciseFreq}
+          <tr className='flex flex-col'>
+            <td className=' px-4 bg-gray-700 text-white font-bold text-black'>
+              Exercise Freq
             </td>
+            <td className='px-4 py-2  h-full truncate'>{exerciseFreq}</td>
           </tr>
           {injuries !== '' ? (
-            <tr className='flex flex-col border border-1'>
-              <td className='bg-bright-secondary px-4 '>Injuries</td>
-              <td className='px-4 py-2 sm:px-6 sm:py-4 whitespace-nowrap bg-white h-full overflow-x-auto'>
-                {injuries}
+            <tr className='flex flex-col'>
+              <td className=' px-4 bg-gray-700 text-white font-bold text-black'>
+                Injuries
               </td>
+              <td className='px-4 py-2  h-full truncate lip'>{injuries}</td>
             </tr>
           ) : null}
+          <tr className='flex flex-col'>
+            <td className=' px-4 bg-gray-700 text-white font-bold text-black'>
+              Goal
+            </td>
+            <td className='px-4 py-2  h-full truncate'>{goal}</td>
+          </tr>
         </tbody>
       </table>
-      <div className='gap-4 col-span-full  text-end mt-8'>
+      <div className='gap-4 col-span-full  text-end mt-8  bottom-16 right-4'>
         <Button
           onClick={onPrevStep}
           type='button'
           styles={
-            'w-24 z-20 bg-bright-secondary rounded-xl py-2 text-white font-normal text-lg col-span-2 w-52 place-self-end'
+            'w-24 z-20 rounded-xl bg-gray-700  py-2 text-white font-normal text-lg col-span-2place-self-end hover:scale-110 transition duration-300 ease-in-out'
           }
-          text='Go back'
+          text='Back'
         />
         <Button
+          onClick={onNextStep}
           type='button'
           styles={
-            'w-24 ml-4 z-20 bg-bright-secondary rounded-xl py-2 text-white font-normal text-lg col-span-2 w-52 place-self-end'
+            'w-24 rounded-xl ml-4 z-20 bg-bright-secondary  py-2 text-white font-normal text-lg col-span-2 place-self-end hover:scale-110 transition duration-300 ease-in-out'
           }
-          text='Next'
+          text='Finish'
         />
       </div>
-    </>
+    </section>
   );
 };
-
 export default Summary;
--- src/components/form-steps/CustomForm.tsx
+++ src/components/form-steps/CustomForm.tsx
@@ -22,11 +22,13 @@ const CustomForm: React.FC<StepOneFormProps> = ({
   } = useCustomForm(onNextStep, onPrevStep);
   return (
     <>
-      <form className='flex flex-col'>
+      <form className='flex flex-col '>
         <fieldset
           className={` grid ${String(gridCols)} gap-8 text-black text-start`}
         >
-          <legend className='w-full text-center mb-8'>{title}</legend>
+          <legend className='w-full text-center mb-8 text-2xl font-bold'>
+            {title}
+          </legend>
           {fields &&
             fields.map((elements) => {
               const uniqueKey = uuidv4();
@@ -34,20 +36,20 @@ const CustomForm: React.FC<StepOneFormProps> = ({
             })}
         </fieldset>
       </form>
-      <div className='gap-4 col-span-full  text-end mt-8'>
+      <div className='flex gap-4 col-span-full text-end flex-1 justify-end pb-4 mt-4'>
         <Button
           onClick={handlePrevStep}
           type='button'
           styles={
-            'w-24 z-20 bg-bright-secondary rounded-xl py-2 text-white font-normal text-lg col-span-2 w-52 place-self-end'
+            'px-4 z-20 bg-gray-700 rounded-lg py-2 text-white font-normal text-lg col-span-2 place-self-end  hover:scale-110 transition duration-300 ease-in-out'
           }
-          text='Go back'
+          text='Back'
         />
         <Button
           onClick={handleSubmit}
           type='button'
           styles={
-            'w-24 ml-4 z-20 bg-bright-secondary rounded-xl py-2 text-white font-normal text-lg col-span-2 w-52 place-self-end'
+            'px-4 ml-4 z-20 bg-bright-secondary rounded-lg py-2 text-white font-normal text-lg col-span-2 place-self-end  hover:scale-110 transition duration-300 ease-in-out'
           }
           text='Next'
         />
