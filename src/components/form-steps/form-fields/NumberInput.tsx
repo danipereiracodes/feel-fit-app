580ce257f10c36aafc1990c2ed6ceba64a573fc4
danipereiracodes <danipg.dev@gmail.com>
2024-04-24 23:29:18 +0200

    01-feature-form-validations / add form fields validations

--- src/components/form-steps/form-fields/NumberInput.tsx
+++ src/components/form-steps/form-fields/NumberInput.tsx
@@ -1,55 +1,54 @@
 import { FieldError, UseFormRegister } from 'react-hook-form';
 import { InputValues } from '../../../types/InputTypes';
 
-interface ControlledInputProps {
+interface NumberInputProps {
   name: string;
   register: UseFormRegister<InputValues>;
   required?: boolean;
-  type: string;
+  min: number;
+  max: number;
   label: string;
-  onBlurHandler: (e: React.FocusEvent<HTMLInputElement>) => void;
-  spanText: string | undefined;
-  placeHolder: string;
+  placeholder: string;
   error?: FieldError;
 }
 
-const ControlledInput = ({
+const NumberInput = ({
   name,
-  onBlurHandler,
   register,
-  spanText,
-  error,
-  type,
   label,
-
-  required,
-}: ControlledInputProps) => {
-  const textDotsandComasPattern = /^[a-zA-Z,.\\s]*$/;
-
-  const validateRequired = required ? `required` : required;
+  min,
+  max,
+  placeholder,
+  error,
+}: NumberInputProps) => {
+  const numPattern = /^[0-9]*$/;
 
   const validationRules = {
-    required: validateRequired,
+    required: `required`,
     pattern: {
-      value: textDotsandComasPattern,
-      message: 'Text characters and comas only',
+      value: numPattern,
+      message: 'only numbers',
+    },
+    min: {
+      value: min,
+      message: `too short`,
+    },
+    max: {
+      value: max,
+      message: `too long`,
     },
   };
 
   return (
-    <div className='relative flex flex-col '>
+    <div className='flex flex-col relative'>
       <label htmlFor={name}>{label}</label>
-
       <input
-        type={type}
+        placeholder={placeholder}
         {...register(name as keyof InputValues, validationRules)}
-        onBlur={onBlurHandler}
-        placeholder={spanText}
         className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
       />
-
       {error && (
-        <p className='absolute -bottom-6 left-0 text-sm text-gray-700 font-bold font-bold'>
+        <p className='absolute -bottom-6 left-0 text-sm text-gray-700 font-bold'>
           *{error.message}
         </p>
       )}
@@ -57,4 +56,4 @@ const ControlledInput = ({
   );
 };
 
-export default ControlledInput;
+export default NumberInput;
--- src/hooks/useCustomForm.tsx
+++ src/hooks/useCustomForm.tsx
@@ -1,20 +1,240 @@
 import { useEffect, useState } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import useStepFields from './useStepFields';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 import { useStepStore } from '../store/StepStore';
 import { InputValues } from '../types/InputTypes';
+import CustomSelect from '../components/form-steps/form-fields/CustomSelect';
+
+import ControlledInput from '../components/form-steps/form-fields/ControlledInput';
+import CustomCheckbox from '../components/form-steps/form-fields/CustomCheckbox';
+import { DietEnum } from '../enums/DietEnums';
+import {
+  ExerciseExpEnum,
+  ExerciseFreqEnum,
+  FitnessGoal,
+} from '../enums/ExerciseExp';
+import { FastingFrequencyEnum } from '../enums/FastingFreqEnum';
+import { GenderEnum } from '../enums/GenderEnums';
+import TextInput from '../components/form-steps/form-fields/TextInput';
+import NumberInput from '../components/form-steps/form-fields/NumberInput';
 
 const useCustomForm = (onNextStep: () => void, onPrevStep: () => void) => {
-  const { register, handleSubmit, control, reset } = useForm<InputValues>();
+  const {
+    register,
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors },
+  } = useForm<InputValues>();
   const [formData, setFormData] = useState<InputValues | null>(null);
-  const { fields } = useStepFields(register, control);
+
   const inputData = useStepStore((state) => state.data);
   const updateData = useStepStore((state) => state.updateData);
 
   const onSubmit: SubmitHandler<InputValues> = (data) => {
     updateData(data);
     onNextStep();
-    console.log('hello');
+  };
+
+  const isFasting = useStepStore((state) => state.data.fasting);
+
+  const handleTextInputBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    const splittedText = inputValue.split(/[,. ]+/);
+    const formattedText = splittedText.join(', ');
+    e.target.value = formattedText;
+  };
+
+  type EnumType =
+    | typeof GenderEnum
+    | typeof DietEnum
+    | typeof FastingFrequencyEnum
+    | typeof ExerciseExpEnum
+    | typeof ExerciseFreqEnum
+    | typeof FitnessGoal;
+
+  const enumToSelectOptions = (enumObject: EnumType) => {
+    return Object.keys(enumObject).map((key) => ({
+      value: enumObject[key as keyof typeof enumObject],
+      name: key,
+    }));
+  };
+
+  const genderOptions = enumToSelectOptions(GenderEnum);
+  const dietOptions = enumToSelectOptions(DietEnum);
+  const fastingOption = enumToSelectOptions(FastingFrequencyEnum);
+  const exerciseOptions = enumToSelectOptions(ExerciseExpEnum);
+  const exerciseFreOptions = enumToSelectOptions(ExerciseFreqEnum);
+  const fitnessGoalOptions = enumToSelectOptions(FitnessGoal);
+
+  const getStepOneFields = () => [
+    <TextInput
+      name='name'
+      label='Name'
+      register={register}
+      required
+      placeholder='Jane'
+      maxLength={15}
+      minLength={3}
+      error={errors.name}
+    />,
+    <TextInput
+      name='lastname'
+      label='Last name'
+      register={register}
+      required={false}
+      placeholder='Doe'
+      maxLength={15}
+      minLength={3}
+      error={errors.lastname}
+    />,
+    <CustomSelect
+      register={register}
+      label='Gender'
+      name='gender'
+      options={genderOptions}
+      error={errors.gender}
+      required
+    />,
+
+    <NumberInput
+      min={14}
+      max={140}
+      name='age'
+      label='How old?'
+      register={register}
+      required
+      placeholder='35'
+      error={errors.age}
+    />,
+    <NumberInput
+      min={40}
+      max={250}
+      name='weight'
+      label='Weight(kg)'
+      register={register}
+      required
+      placeholder='50'
+      error={errors.weight}
+    />,
+    <NumberInput
+      min={40}
+      max={240}
+      name='height'
+      label='Height(cm)'
+      register={register}
+      required
+      placeholder='162'
+      error={errors.height}
+    />,
+  ];
+
+  const getStepTwoFields = () => [
+    <CustomSelect
+      register={register}
+      label='How you eat'
+      name='diet'
+      options={dietOptions}
+      error={errors.diet}
+      required
+    />,
+    <Controller
+      control={control}
+      name='allergies'
+      render={() => (
+        <ControlledInput
+          type='text'
+          label='Any allergies?'
+          register={register}
+          onBlurHandler={handleTextInputBlur}
+          name='allergies'
+          spanText='Add them separated by comas or dots'
+          placeHolder='gluten,soy...'
+          error={errors.allergies}
+          required={false}
+        />
+      )}
+    />,
+    <Controller
+      control={control}
+      name='fasting'
+      render={() => (
+        <CustomCheckbox
+          required={false}
+          register={register}
+          title='I practice intermintent fasting'
+          type='checkbox'
+          name='fasting'
+        />
+      )}
+    />,
+    <CustomSelect
+      register={register}
+      label='How often?'
+      name='fastingFreq'
+      options={fastingOption}
+      disabled={!isFasting}
+      error={errors.fastingFreq}
+      required={false}
+    />,
+  ];
+
+  const getStepThreeFields = () => [
+    <CustomSelect
+      register={register}
+      label='Exercise experience'
+      name='exerciseExp'
+      options={exerciseOptions}
+      error={errors.exerciseExp}
+      required
+    />,
+
+    <CustomSelect
+      register={register}
+      label='How often?'
+      name='exerciseFreq'
+      options={exerciseFreOptions}
+      error={errors.exerciseFreq}
+      required
+    />,
+    <Controller
+      control={control}
+      name='injuries'
+      render={() => (
+        <ControlledInput
+          type='text'
+          label='Any injuries?'
+          register={register}
+          onBlurHandler={handleTextInputBlur}
+          name='injuries'
+          spanText='Add them separated by comas or dots'
+          placeHolder='back hernia...'
+          required={false}
+          error={errors.injuries}
+        />
+      )}
+    />,
+    <CustomSelect
+      register={register}
+      label='Fitness Goal?'
+      name='goal'
+      options={fitnessGoalOptions}
+      error={errors.goal}
+      required
+    />,
+  ];
+
+  const renderSteps = (step: number) => {
+    switch (step) {
+      case 1:
+        return getStepOneFields();
+      case 2:
+        return getStepTwoFields();
+      case 3:
+        return getStepThreeFields();
+
+      default:
+        break;
+    }
   };
 
   useEffect(() => {
@@ -37,7 +257,7 @@ const useCustomForm = (onNextStep: () => void, onPrevStep: () => void) => {
   }, [formData, reset]);
 
   return {
-    fields,
+    renderSteps,
     handleSubmit: handleSubmit(onSubmit),
     onPrevStep,
   };
