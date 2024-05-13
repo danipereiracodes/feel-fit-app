5d2aadbe47a91f603a83584f32a7c8ab4d85f236
danipereiracodes <info.danipereira@gmail.com>
2024-05-13 17:56:46 +0200

    01-fix-summary/edit header styles

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,15 +1,17 @@
 const Header: React.FC = () => {
   return (
-    <header className='flex items-center justify-center bg-secondary-dark text-primary-gray font-extrabold text-2xl'>
-      <a href='/'>
-        <div className='flex gap-2 items-center'>
-          <img
-            className='w-[125px]'
-            src='./images/logo/fee-fit-logo.png'
-            alt='feel fit logo'
-          />
-        </div>
-      </a>
+    <header className='flex items-center bg-secondary-dark text-primary-gray font-extrabold text-2xl'>
+      <div className='container mx-auto'>
+        <a href='/'>
+          <div className='flex gap-2 items-center'>
+            <img
+              className='w-[125px]'
+              src='./images/logo/fee-fit-logo.png'
+              alt='feel fit logo'
+            />
+          </div>
+        </a>
+      </div>
     </header>
   );
 };
